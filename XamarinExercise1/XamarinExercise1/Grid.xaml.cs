b3f5d22bd84111bd479506d5b690b2e5ca069dad
Elevation07 <leodekit97@gmail.com>
2020-07-27 23:48:37 +0800

    Xamarin Excercise

---
+++ XamarinExercise1/XamarinExercise1/Grid.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace XamarinExercise1
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Grid : ContentPage
+    {
+        static int ButtonCount = 0;
+        public Grid()
+        {
+            InitializeComponent();
+           
+        }
+        
+
+private void Button_Clicked(object sender, EventArgs e)
+        {
+           
+             
+                if (ButtonCount < 1)
+                {
+                    
+                   
+                labelText.Text = "Xamarin.Forms is an open source cross-platform framework from Microsoft for building iOS, Android, & Windows apps with .NET from a single shared codebase.";
+                ButtonCount++;
+            }
+                else
+                {
+               
+                labelText.Text = "I am a Xamarin Developer";
+                ButtonCount = 0;
+
+            }
+               
+
+
+
+        }
+    }
+}
---
+++ XamarinExercise1/XamarinExercise1/MainPage.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace XamarinExercise1
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+        }
+
+        async void Button_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new StacklayoutBasicPage());
+        }
+
+        async void Button_Clicked_1(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new LayoutOptions());
+        }
+
+        async void Button_Clicked_2(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new StackLayoutSample());
+        }
+
+        async void Button_Clicked_3(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Grid());
+        }
+    }
+}
